5f6f8914a6d3a216bb582ba7f524f8b657f6b1b7
KyerrreN <kyerrrenmgt@gmail.com>
2024-06-03 18:57:53 +0300

    feat: hamburger menu disables scrolling

--- JS/HamburgerMenu.js
+++ JS/HamburgerMenu.js
@@ -5,4 +5,10 @@ const offScreenMenu = document.querySelector(".off-screen-menu");
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
+
+    if (hamMenu.classList.contains("active")) {
+        document.body.style.overflow = "hidden";
+    } else {
+        document.body.style.overflow = "auto";
+    }
 });
