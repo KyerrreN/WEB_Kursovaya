efbc5fc344046d447170eb2795bff8cf59c3210c
KyerrreN <kyerrrenmgt@gmail.com>
2024-06-08 13:25:28 +0300

    feat: fully functional pagination with data fetched from json file + adaptive layout

--- JS/Pagination.js
+++ JS/Pagination.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let jsonPagination;
+
     let paginationNumber = document.getElementsByClassName("pagination-number");
     let currentValue = 0;
 
@@ -7,35 +9,70 @@ document.addEventListener("DOMContentLoaded", () => {
     let paginationThree = document.getElementById("pagination-number-three");
     let paginationFour = document.getElementById("pagination-number-four");
 
+    const contentHeader = document.querySelector(
+        ".news-pagination-content-header"
+    );
+    const contentText = document.querySelector(".news-pagination-content-text");
+    const contentBackground = document.querySelector(
+        ".news-pagination-content"
+    );
+
+    function changeContentInPagination(index) {
+        contentHeader.innerHTML = jsonPagination[index].header;
+        contentText.innerHTML = jsonPagination[index].text;
+        contentBackground.style.backgroundImage = `url(${jsonPagination[index].picture})`;
+    }
+
+    fetch("../JSON/News.json")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Fetch error: " + response.statusText);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            jsonPagination = data;
+            changeContentInPagination(0);
+        })
+        .catch((error) =>
+            console.error(
+                "Error while fetching the data from local JSON file ",
+                error
+            )
+        );
+
     function removeActiveLi() {
         for (num of paginationNumber) {
             num.classList.remove("active");
         }
     }
-
     paginationOne.addEventListener("click", () => {
         removeActiveLi();
 
         currentValue = 0;
         paginationNumber[currentValue].classList.add("active");
+        changeContentInPagination(currentValue);
     });
     paginationTwo.addEventListener("click", () => {
         removeActiveLi();
 
         currentValue = 1;
         paginationNumber[currentValue].classList.add("active");
+        changeContentInPagination(currentValue);
     });
     paginationThree.addEventListener("click", () => {
         removeActiveLi();
 
         currentValue = 2;
         paginationNumber[currentValue].classList.add("active");
+        changeContentInPagination(currentValue);
     });
     paginationFour.addEventListener("click", () => {
         removeActiveLi();
 
         currentValue = 3;
         paginationNumber[currentValue].classList.add("active");
+        changeContentInPagination(currentValue);
     });
 
     let paginationLeft = document.querySelector("#pagination-left");
@@ -47,6 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             currentValue--;
             paginationNumber[currentValue].classList.add("active");
+            changeContentInPagination(currentValue);
         }
     });
 
@@ -56,6 +94,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             currentValue++;
             paginationNumber[currentValue].classList.add("active");
+            changeContentInPagination(currentValue);
         }
     });
 
@@ -68,6 +107,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             currentValue = 0;
             paginationNumber[currentValue].classList.add("active");
+            changeContentInPagination(currentValue);
         }
     });
 
@@ -77,6 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             currentValue = 3;
             paginationNumber[currentValue].classList.add("active");
+            changeContentInPagination(currentValue);
         }
     });
 });
