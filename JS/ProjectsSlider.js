1a695613d38c8206b64c55b408d407dfc4bc3c64
KyerrreN <kyerrrenmgt@gmail.com>
2024-06-08 19:10:11 +0300

    feat: working slider without randomization

--- JS/ProjectsSlider.js
+++ JS/ProjectsSlider.js
@@ -16,3 +16,40 @@ let projectsSlider = new Swiper(".swiper", {
     initialSlides: 1,
     loop: true,
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    let sliderElements = document.querySelectorAll(".swiper-slide-container");
+    let sliderElementsText = document.querySelectorAll(
+        ".swiper-slide-container-text"
+    );
+    let jsonData;
+
+    fetch("../JSON/Projects.json")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Fetch error: " + response.statusText);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            jsonData = data;
+
+            changeSliderElements();
+        })
+        .catch((error) =>
+            console.error(
+                "Error while fetching the data from local JSON file ",
+                error
+            )
+        );
+
+    function changeSliderElements() {
+        for (let i = 0; i < sliderElements.length; i++) {
+            // sliderElements[i].innerHTML = jsonData[i].text;
+            sliderElements[
+                i
+            ].style.backgroundImage = `url(${jsonData[i].image})`;
+            sliderElementsText[i].innerHTML = jsonData[i].text;
+        }
+    }
+});
