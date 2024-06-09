660296db6bfbc5a4c529868f324bfa0140221303
KyerrreN <kyerrrenmgt@gmail.com>
2024-06-09 16:38:34 +0300

    feat: add clear local storage functionality

---
+++ JS/AdminPanelClearLS.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const clearLocalButton = document.querySelector("#admin-intro-clear-local");
+
+    clearLocalButton.addEventListener("click", () => {
+        localStorage.clear();
+
+        alert("You will be redirected to the main page");
+        window.location.href = "../index.html";
+    });
+});
