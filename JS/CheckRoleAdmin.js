8cca3d5936efb556fbbb4d188fc4c7cdb59f910a
KyerrreN <kyerrrenmgt@gmail.com>
2024-06-09 16:20:25 +0300

    feat: deny entry to admin panel if not admin

---
+++ JS/CheckRoleAdmin.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function isEntryAllowed() {
+        if (localStorage.getItem("Logged") === null) {
+            return false;
+        }
+
+        const checkedUserData = JSON.parse(localStorage.getItem("Logged"));
+
+        if (checkedUserData.role !== "admin") {
+            return false;
+        }
+
+        return true;
+    }
+
+    if (!isEntryAllowed()) {
+        window.location.href = "../index.html";
+    }
+});
