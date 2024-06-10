b2330e23a9652aeb78c17500d6cefffe2cd9e9b3
KyerrreN <kyerrrenmgt@gmail.com>
2024-06-10 10:40:40 +0300

    feat: admin panel is now fully translated

--- TranslationJS/AdminTranslate.js
+++ TranslationJS/AdminTranslate.js
@@ -18,15 +18,11 @@ function changeLanguage(translations) {
             }
         }
     }
-
-    const getTemplateAlign = document.querySelector(
-        ".header-content-nav-rectangle-text"
-    );
-    getTemplateAlign.style.textAlign = "center";
 }
 document.addEventListener("DOMContentLoaded", () => {
     const translations = {
         en: {
+            //modal
             ".modal-header":
                 "Here is your profile information. In case you forgot any of your credentials, you can always look them up here.",
             ".modal-nickname": "Nickname",
@@ -38,13 +34,41 @@ document.addEventListener("DOMContentLoaded", () => {
             ".modal-pass": "Password",
             ".modal-role": "Role",
 
+            //footer
             ".footer-content-text-desc-powered": "Powered by Webflow",
             "#footer-content-text-desc-powered-second":
                 "All rights reserved Wishbone+Partners, Inc.Licensing",
             ".footer-content-text-button-language": "Change language",
             ".footer-content-text-button-theme": "Change theme",
+
+            //ham + header
+            "#off-screen-menu-logout": "Log out",
+            "#off-screen-menu-login": "Login",
+            "#ham-admin": "Admin panel",
+            "#ham-main-page": "Main Page",
+            "#ham-news": "News",
+            ".header-content-nav-rectangle": "Get template",
+            ".header-content-nav-logout": "Log out",
+            ".header-content-nav-admin": "Admin panel",
+            ".header-content-nav-login": "Login",
+            ".header-content-nav-main-page": "Main Page",
+            ".header-content-nav-news": "News",
+
+            //main
+            "#admin-intro-header":
+                "Here is your admin panel, where you can see all user information, located in the local storage. You can also",
+            "#admin-intro-clear-local": "clear local storage.",
+            "#th-first": "Nickname",
+            "#th-second": "First name",
+            "#th-third": "Last name",
+            "#th-fourth": "Patronymic",
+            "#th-fivth": "Password",
+            "#th-sixth": "Phone",
+            "#th-seventh": "Date of birth",
+            "#th-eighth": "Role",
         },
         ru: {
+            //modal
             ".modal-header":
                 "Здесь находятся ваши персональные данные. На случай если вы забыли учетные данные, вы всегда можете посмотреть их здесь.",
             ".modal-nickname": "Никнейм",
@@ -56,11 +80,38 @@ document.addEventListener("DOMContentLoaded", () => {
             ".modal-pass": "Пароль",
             ".modal-role": "Роль",
 
+            //footer
             ".footer-content-text-desc-powered": "Предоставлено от Webflow",
             "#footer-content-text-desc-powered-second":
                 "Все права защищены Wishbone+Partners, Inc.Licensing",
             ".footer-content-text-button-language": "Сменить язык",
             ".footer-content-text-button-theme": "Сменить тему",
+
+            //ham + header
+            "#off-screen-menu-logout": "Выйти",
+            "#off-screen-menu-login": "Войти",
+            "#ham-admin": "Админ панель",
+            "#ham-main-page": "Главная",
+            "#ham-news": "Новости",
+            ".header-content-nav-rectangle": "Получить набросок",
+            ".header-content-nav-logout": "Выйти",
+            ".header-content-nav-admin": "Админ панель",
+            ".header-content-nav-login": "Войти",
+            ".header-content-nav-news": "Новости",
+            ".header-content-nav-main-page": "Главная",
+
+            //main
+            "#admin-intro-header":
+                "Здесь находится ваша админ панель, где вы можете посмотреть всю информацию о пользователях в локальном хранилище. Вы также можете",
+            "#admin-intro-clear-local": "его очистить.",
+            "#th-first": "Никнейм",
+            "#th-second": "Имя",
+            "#th-third": "Фамилия",
+            "#th-fourth": "Отчество",
+            "#th-fivth": "Пароль",
+            "#th-sixth": "Телефон",
+            "#th-seventh": "Дата рождения",
+            "#th-eighth": "Роль",
         },
     };
 
