f2be654c24f700aaa1e06c16e9f0028aa8a2bc55
KyerrreN <kyerrrenmgt@gmail.com>
2024-06-10 10:23:03 +0300

    get in touch fully translated

--- JS/LocationEmail.js
+++ JS/LocationEmail.js
@@ -1,12 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
     let inputElement = document.querySelector(".maps-socials-email-input");
-    inputElement.placeholder = "Enter your message here";
+    let placeholderMessage = changePlaceholderLanguage();
+    const buttonToChangePlaceholder = document.querySelector(
+        ".footer-content-text-button-language"
+    );
+
+    buttonToChangePlaceholder.addEventListener("click", () => {
+        placeholderMessage = changePlaceholderLanguage();
+    });
+
+    inputElement.placeholder = placeholderMessage;
 
     inputElement.addEventListener("input", () => {
         if (inputElement.value.trim() !== "") {
             inputElement.placeholder = "";
         } else {
-            inputElement.placeholder = "Enter your message here";
+            inputElement.placeholder = changePlaceholderLanguage();
         }
     });
 });
+
+function changePlaceholderLanguage() {
+    let placeholderMsg;
+
+    if (
+        localStorage.getItem("lang") === "en" ||
+        localStorage.getItem("lang") === null
+    ) {
+        placeholderMsg = "Enter your email here";
+    } else {
+        placeholderMsg = "Здесь вводится адрес почты";
+    }
+
+    return placeholderMsg;
+}
--- TranslationJS/LocationTranslate.js
+++ TranslationJS/LocationTranslate.js
@@ -1,5 +1,9 @@
 function changeLanguage(translations) {
     let currentLanguage;
+    const placeholderInInput = document.querySelector(
+        ".maps-socials-email-input"
+    );
+
     if (localStorage.getItem("lang") === null) {
         currentLanguage = "ru";
         localStorage.setItem("lang", "ru");
@@ -23,10 +27,17 @@ function changeLanguage(translations) {
         ".header-content-nav-rectangle-text"
     );
     getTemplateAlign.style.textAlign = "center";
+
+    if (currentLanguage === "en") {
+        placeholderInInput.placeholder = "Enter your email here";
+    } else {
+        placeholderInInput.placeholder = "Здесь вводится адрес почты";
+    }
 }
 document.addEventListener("DOMContentLoaded", () => {
     const translations = {
         en: {
+            //modal
             ".modal-header":
                 "Here is your profile information. In case you forgot any of your credentials, you can always look them up here.",
             ".modal-nickname": "Nickname",
@@ -38,13 +49,42 @@ document.addEventListener("DOMContentLoaded", () => {
             ".modal-pass": "Password",
             ".modal-role": "Role",
 
+            //footer
             ".footer-content-text-desc-powered": "Powered by Webflow",
             "#footer-content-text-desc-powered-second":
                 "All rights reserved Wishbone+Partners, Inc.Licensing",
             ".footer-content-text-button-language": "Change language",
             ".footer-content-text-button-theme": "Change theme",
+
+            //ham + header
+            "#off-screen-menu-logout": "Log out",
+            "#off-screen-menu-login": "Login",
+            "#ham-admin": "Admin panel",
+            "#ham-main-page": "Main Page",
+            "#ham-news": "News",
+            ".header-content-nav-rectangle": "Get template",
+            ".header-content-nav-logout": "Log out",
+            ".header-content-nav-admin": "Admin panel",
+            ".header-content-nav-login": "Login",
+            ".header-content-nav-main-page": "Main Page",
+            ".header-content-nav-news": "News",
+            ".header-content-nav-rectangle-text": "Get template",
+
+            //main
+            "#maps-fb": "Our X: @WishbonePartners",
+            "#maps-x": "Our FB: @wishbone",
+            "#maps-inst": "Our inst: @wishbone__",
+            "#maps-socials-header-upper":
+                "If you'd like to contact us, be sure to visit our socials",
+            "#maps-socials-header-lower":
+                "Or send us your email, we will contact you ourselves!",
+            ".maps-map-header":
+                "You may also come to us in person, where we will show you all our projects in person and consult you regarding any aspect you may be concerned about.",
+            "#maps-map-embedding-topoint": "We are here",
+            ".maps-socials-email-button": "Send",
         },
         ru: {
+            //modal
             ".modal-header":
                 "Здесь находятся ваши персональные данные. На случай если вы забыли учетные данные, вы всегда можете посмотреть их здесь.",
             ".modal-nickname": "Никнейм",
@@ -56,11 +96,39 @@ document.addEventListener("DOMContentLoaded", () => {
             ".modal-pass": "Пароль",
             ".modal-role": "Роль",
 
+            //footer
             ".footer-content-text-desc-powered": "Предоставлено от Webflow",
             "#footer-content-text-desc-powered-second":
                 "Все права защищены Wishbone+Partners, Inc.Licensing",
             ".footer-content-text-button-language": "Сменить язык",
             ".footer-content-text-button-theme": "Сменить тему",
+
+            //ham + header
+            "#off-screen-menu-logout": "Выйти",
+            "#off-screen-menu-login": "Войти",
+            "#ham-admin": "Админ панель",
+            "#ham-main-page": "Главная",
+            "#ham-news": "Новости",
+            ".header-content-nav-rectangle": "Получить набросок",
+            ".header-content-nav-logout": "Выйти",
+            ".header-content-nav-admin": "Админ панель",
+            ".header-content-nav-login": "Войти",
+            ".header-content-nav-news": "Новости",
+            ".header-content-nav-main-page": "Главная",
+            ".header-content-nav-rectangle-text": "Получить набросок",
+
+            //main
+            "#maps-fb": "Наш Х: @WishbonePartners",
+            "#maps-x": "Наш фейсбук: @wishbone",
+            "#maps-inst": "Наш инстаграм: @wishbone__",
+            "#maps-socials-header-upper":
+                "Если вы хотите связаться с нами, не стесняйтесь посетить наши социальные сети",
+            "#maps-socials-header-lower":
+                "Или перешлите ваш адрес электронной почты, и мы свяжемся с вами!",
+            ".maps-map-header":
+                "Вы также можете посетить нас лично, где мы покажем вам все наши проекты и проконсультируем касательно всех вопросов, которые могут возникнуть.",
+            "#maps-map-embedding-topoint": "Мы здесь",
+            ".maps-socials-email-button": "Отправить",
         },
     };
 
