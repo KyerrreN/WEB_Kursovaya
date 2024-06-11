0e421ceda02aed8417f01c34658b0d6e42f64b09
KyerrreN <kyerrrenmgt@gmail.com>
2024-06-11 17:32:39 +0300

    fix: clearing local storage doesnt invert language settings and logs admin out

--- JS/AdminPanelClearLS.js
+++ JS/AdminPanelClearLS.js
@@ -2,8 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const clearLocalButton = document.querySelector("#admin-intro-clear-local");
 
     clearLocalButton.addEventListener("click", () => {
-        localStorage.clear();
+        for (let i = localStorage.length - 1; i >= 0; i--) {
+            if (!isNaN(Number(localStorage.key(i)))) {
+                localStorage.removeItem(localStorage.key(i));
+            }
+        }
 
+        localStorage.removeItem("Logged");
         alert("You will be redirected to the main page");
         window.location.href = "index.html";
     });
